c315690296389b33b00f67fdbd8c1cb9937f702b
Jason Schmitt <schmitty890@gmail.com>
2021-03-17 19:57:23 -0400

    testing images in prod

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,75 @@
 import React, { Component } from "react"
-import { ChakraProvider, Box } from "@chakra-ui/react"
-import { UserProvider, UserConsumer } from "../contexts/UserContext"
-import { SecondProvider, SecondConsumer } from "../contexts/SecondContext"
-import TestHolder from "../components/testHolder/testHolder"
-import Header from "../components/header/header"
+import {
+  ChakraProvider,
+  Box,
+  Container,
+  Grid,
+  GridItem,
+  Wrap,
+  WrapItem,
+  Center,
+  Flex,
+  Avatar,
+  Text,
+  Badge,
+} from "@chakra-ui/react"
 
 export default function Home() {
   return (
     <ChakraProvider>
-      <UserProvider>
-        <SecondProvider>
-          <Header />
-          <Box bg="" w="100%" p={4} color="green">
-            <div>Hello world!</div>
-            <TestHolder />
-          </Box>
-        </SecondProvider>
-      </UserProvider>
+      <Container maxW="4xl" centerContent>
+        <Box padding="4" bg="gray.100" maxW="4xl" width="100%">
+          <Grid
+            h="200px"
+            templateRows="repeat(2, 1fr)"
+            templateColumns="repeat(12, 1fr)"
+            gap={4}
+          >
+            <GridItem
+              p={4}
+              rowSpan={1}
+              colSpan={{ base: 12 }}
+              bg={{ base: "white" }}
+            >
+              <Flex>
+                <Avatar src="/images/jasonschmitt.jpg" />
+                <Box ml="3">
+                  <Text fontWeight="bold">
+                    Jason Schmitt
+                    <Badge ml="1" colorScheme="green">
+                      New
+                    </Badge>
+                  </Text>
+                  <Text fontSize="sm">UI Engineer</Text>
+                </Box>
+              </Flex>
+            </GridItem>
+            <GridItem
+              rowSpan={1}
+              colSpan={{ base: 12, md: 6, lg: 4 }}
+              bg={{ base: "black", md: "green", lg: "blue" }}
+            >
+              here is text
+            </GridItem>
+            <GridItem
+              rowSpan={1}
+              colSpan={{ base: 12, md: 6, lg: 4 }}
+              bg={{ base: "black", md: "green", lg: "green" }}
+              centerContent
+            >
+              here is text
+            </GridItem>
+            <GridItem
+              rowSpan={1}
+              colSpan={{ base: 12, md: 6, lg: 4 }}
+              bg={{ base: "black", md: "green", lg: "red" }}
+              centerContent
+            >
+              here is text
+            </GridItem>
+          </Grid>
+        </Box>
+      </Container>
     </ChakraProvider>
   )
 }
-
-// class Home extends Component {
-
-//   render() {
-//     return (
-//       <ChakraProvider>
-//         <UserProvider>
-//             <Box bg="" w="100%" p={4} color="green">
-//               <div>Hello world!</div>
-//               <TestHolder />
-//             </Box>
-//         </UserProvider>
-//       </ChakraProvider>
-//     )
-//   }
-// }
-// export default Home
