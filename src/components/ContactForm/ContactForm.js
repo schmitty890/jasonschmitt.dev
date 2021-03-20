acfcf77780f4664f881236444aada9af5b1710d7
Jason Schmitt <schmitty890@gmail.com>
2021-03-20 16:09:16 -0400

    form validation and creation

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -2,19 +2,31 @@ import React, { useState } from "react"
 
 import { useFormik } from "formik"
 
-import { FormLabel, Grid, Input, GridItem, Button } from "@chakra-ui/react"
+import {
+  FormLabel,
+  Grid,
+  Input,
+  GridItem,
+  Textarea,
+  Button,
+  Text,
+  FormControl,
+} from "@chakra-ui/react"
 
 const ContactForm = () => {
   const [isLoading, setIsLoading] = useState(false)
 
   function validate(values) {
     const errors = {}
-    if (!values.favoriteFood) {
-      errors.favoriteFood = "Required"
+    if (!values.yourName) {
+      errors.yourName = "Required"
     }
 
-    if (!values.favoritePlace) {
-      errors.favoritePlace = "Required"
+    if (!values.yourEmail) {
+      errors.yourEmail = "Required"
+    }
+    if (!values.yourMessage) {
+      errors.yourMessage = "Say something"
     }
     return errors
   }
@@ -28,8 +40,9 @@ const ContactForm = () => {
     errors,
   } = useFormik({
     initialValues: {
-      favoriteFood: "",
-      favoritePlace: "",
+      yourName: "",
+      yourEmail: "",
+      yourMessage: "",
     },
     validate,
     onSubmit: values => {
@@ -39,7 +52,7 @@ const ContactForm = () => {
         console.log(JSON.stringify(values))
         setIsLoading(false)
       }, 2000)
-      // values = {"favoriteFood":"ramen","favoritePlace":"mountains"}
+      // values = {"yourName":"ramen","yourEmail":"mountains"}
     },
   })
 
@@ -47,28 +60,52 @@ const ContactForm = () => {
     <Grid templateColumns="repeat(12, 1fr)" gap={0.5}>
       <GridItem rowSpan={1} colSpan={{ base: 12 }} bg={{ base: "white" }} p={4}>
         <form onSubmit={handleSubmit}>
-          <FormLabel htmlFor="favoriteFood">Favorite Food:</FormLabel>
-          <Input
-            type="text"
-            name="favoriteFood"
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          {touched.favoriteFood && errors.favoriteFood ? (
-            <div>{errors.favoriteFood}</div>
-          ) : null}
+          <FormControl isRequired>
+            <FormLabel mt={4} htmlFor="yourName">
+              Your name
+            </FormLabel>
+            <Input
+              type="text"
+              name="yourName"
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {touched.yourName && errors.yourName ? (
+              <Text color="red.500">{errors.yourName}</Text>
+            ) : null}
+          </FormControl>
 
-          <FormLabel htmlFor="favoritePlace">Favorite place:</FormLabel>
-          <Input
-            type="text"
-            name="favoritePlace"
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
+          <FormControl isRequired>
+            <FormLabel mt={4} htmlFor="yourEmail">
+              Your email:
+            </FormLabel>
+            <Input
+              type="text"
+              name="yourEmail"
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
 
-          {touched.favoritePlace && errors.favoritePlace ? (
-            <div>{errors.favoritePlace}</div>
-          ) : null}
+            {touched.yourEmail && errors.yourEmail ? (
+              <Text color="red.500">{errors.yourEmail}</Text>
+            ) : null}
+          </FormControl>
+
+          <FormControl isRequired>
+            <FormLabel mt={4} htmlFor="yourMessage">
+              Your message
+            </FormLabel>
+            <Textarea
+              name="yourMessage"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              mt={4}
+              placeholder="Contact me with any questions or opportunities, or just say whats up!"
+            />
+            {touched.yourMessage && errors.yourMessage ? (
+              <Text color="red.500">{errors.yourMessage}</Text>
+            ) : null}
+          </FormControl>
 
           <Button mt={4} colorScheme="teal" isLoading={isLoading} type="submit">
             submit
