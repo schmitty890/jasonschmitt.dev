17099d339792bc086f18ada6f2b90260f42929f9
Jason Schmitt <schmitty890@gmail.com>
2021-03-20 23:48:45 -0400

    undo domain name

--- src/api/email.js
+++ src/api/email.js
@@ -5,7 +5,7 @@ export const postEmailRoute = async data => {
     let baseURL =
       window.location.hostname === "localhost"
         ? "http://localhost:8081"
-        : "https://www.jasonsprojects.com"
+        : "https://jasonschmitt-dev-api-9w29o.ondigitalocean.app"
     // post data to a url endpoint
     const response = await axios.post(`${baseURL}/email`, data)
     console.log(response)
