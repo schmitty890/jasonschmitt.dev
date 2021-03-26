d9597d40fa76fdffe832bdb9a83052e4375873e4
Jason Schmitt <schmitty890@gmail.com>
2021-03-26 01:05:18 -0400

    adding signup for users

--- src/projects/test/api/signup.js
+++ src/projects/test/api/signup.js
@@ -10,3 +10,38 @@ export const test = async () => {
   }
   return dataObj
 }
+
+export const signUp = async newUser => {
+  console.log("sign up")
+  console.log(newUser)
+  let baseURL =
+    window.location.hostname === "localhost"
+      ? "http://localhost:8081"
+      : "https://jasonschmitt-dev-api-9w29o.ondigitalocean.app"
+
+  // const dataObj = {
+  //   firstName: this.state.firstName,
+  //   email: this.state.email,
+  //   password: this.state.password,
+  // }
+  axios
+    .post(`${baseURL}/auth/register`, newUser)
+    .then(function (response) {
+      console.log(response)
+      localStorage.setItem("token", response.data.token)
+      console.log(typeof response.data._id)
+      localStorage.setItem("_id", response.data._id)
+      console.log("setExpirationInLocalStorage")
+      var expiration = Date.now()
+      var oneMinuteInMilliseconds = 60000
+      // console.log(expiration + oneMinuteInMilliseconds);
+      // console.log(expiration);
+      expiration = expiration + oneMinuteInMilliseconds
+      localStorage.setItem("expire", expiration)
+      console.log("redirect to /dashboard")
+      // window.location.href = '/dashboard'
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+}
--- src/projects/test/contexts/SignUpContext.js
+++ src/projects/test/contexts/SignUpContext.js
@@ -8,8 +8,9 @@ const { Provider, Consumer } = React.createContext()
 
 class SignUpProvider extends Component {
   state = {
-    uiRepoLastUpdated: "a day ago",
-    apiRepoLastUpdated: "a day ago",
+    firstName: "",
+    email: "",
+    password: "",
     test: "ourTextExample",
   }
 
@@ -23,72 +24,16 @@ class SignUpProvider extends Component {
     const testHere = await test()
     console.log(testHere)
     this.setState({ test: testHere.data })
-    this.submitNewUser()
     console.log("new user submitted")
   }
-
-  submitNewUser = async () => {
-    let baseURL =
-      window.location.hostname === "localhost"
-        ? "http://localhost:8081"
-        : "https://jasonschmitt-dev-api-9w29o.ondigitalocean.app"
-
-    const dataObj = {
-      firstName: "Jason",
-      email: "test@gmail.com",
-      password: "mypassword",
-    }
-    axios
-      .post(`${baseURL}/auth/register`, dataObj)
-      .then(function (response) {
-        console.log(response)
-        localStorage.setItem("token", response.data.token)
-        console.log(typeof response.data._id)
-        localStorage.setItem("_id", response.data._id)
-        console.log("setExpirationInLocalStorage")
-        var expiration = Date.now()
-        var oneMinuteInMilliseconds = 60000
-        // console.log(expiration + oneMinuteInMilliseconds);
-        // console.log(expiration);
-        expiration = expiration + oneMinuteInMilliseconds
-        localStorage.setItem("expire", expiration)
-        console.log("redirect to /dashboard")
-        // window.location.href = '/dashboard'
-      })
-      .catch(function (error) {
-        console.log(error)
-      })
-  }
-
-  // getGithubData = async () => {
-  //   const githubData = await listGithubRepos()
-
-  //   let URLsForPorfolioUiAndAPI = ["jasonschmitt.dev", "jasonschmitt.dev-api"]
-
-  //   const dataObj = {
-  //     uiRepoLastUpdated: "",
-  //     apiRepoLastUpdated: "",
-  //   }
-  //   if (githubData != undefined) {
-  //     githubData.data.forEach(arrayItem => {
-  //       if (arrayItem.name == URLsForPorfolioUiAndAPI[0]) {
-  //         dataObj.uiRepoLastUpdated = dayjs(arrayItem.updated_at).fromNow()
-  //       } else if (arrayItem.name == URLsForPorfolioUiAndAPI[1]) {
-  //         dataObj.apiRepoLastUpdated = dayjs(arrayItem.updated_at).fromNow()
-  //       }
-  //     })
-  //     console.log(dataObj)
-  //     this.setState({ uiRepoLastUpdated: dataObj.uiRepoLastUpdated })
-  //     this.setState({ apiRepoLastUpdated: dataObj.apiRepoLastUpdated })
-  //   }
-
-  //   // console.log(githubData)
-  // }
 
   render() {
     return (
       <Provider
         value={{
+          firstName: this.state.firstName,
+          email: this.state.email,
+          password: this.state.password,
           test: this.state.test,
         }}
       >
