cfd6468247c3605acecbc640f45f2d8dbe40d893
Jason Schmitt <schmitty890@gmail.com>
2021-03-30 20:01:29 -0400

    adding playlist project placeholder

---
+++ src/pages/projects/playlist/index.js
@@ -0,0 +1,41 @@
+import React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Container,
+  Grid,
+  Text,
+  GridItem,
+  Heading,
+} from "@chakra-ui/react"
+
+export default function Playlist() {
+  return (
+    <ChakraProvider>
+      <Container maxW="4xl" centerContent>
+        <Box padding="2" bg="gray.100" maxW="4xl" width="100%">
+          <Grid
+            templateRows="repeat(2, 1fr)"
+            templateColumns="repeat(12, 1fr)"
+            gap={0.5}
+            borderRadius="5px"
+          >
+            <GridItem
+              p={4}
+              rowSpan={6}
+              colSpan={{ base: 12 }}
+              bg={{ base: "white" }}
+              justifyContent={"space-between"}
+              display={"inline-flex"}
+            >
+              <Box>
+                <Heading mb={4}>Projects</Heading>
+                <Text fontSize="xl">Playlist here</Text>
+              </Box>
+            </GridItem>
+          </Grid>
+        </Box>
+      </Container>
+    </ChakraProvider>
+  )
+}
